703931ba9f5e3e90281f8b9c1fefac331ddfb44e
pydev455 <pydeveloper455@gmail.com>
2018-12-29 20:18:13 +0530

    added imports

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 ''' flask app with mongo '''
 import os
 import json
+import datetime
+from flask import Flask
+from flask import render_template, request
+# create the flask object
+app = Flask(__name__)
