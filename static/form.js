1cab75b85b6ed01469ed177b5c68c4538ab87716
pydev455 <pydeveloper455@gmail.com>
2019-01-07 20:16:11 +0530

    updated nav heading

--- static/form.js
+++ static/form.js
@@ -8,6 +8,10 @@ function TopHeading(p){
 
 function NavHeading(p){
     return <div className="navbar-header">
+          <button type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
           <a className="navbar-brand" href="#">{p.heading}</a>
         </div>;
