dc0f2da398c332646f678aa8059ffd2b5026264b
pydev455 <pydeveloper455@gmail.com>
2019-01-07 20:15:25 +0530

    added method for top navigation bar

--- static/form.js
+++ static/form.js
@@ -5,3 +5,11 @@ const e = React.createElement;
 function TopHeading(p){
     return <h2 className={p.c}>{p.t}</h2>;
 }
+
+function NavHeading(p){
+    return <div className="navbar-header">
+          </button>
+          <a className="navbar-brand" href="#">{p.heading}</a>
+        </div>;
+
+}
